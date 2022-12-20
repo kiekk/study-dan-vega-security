829ef98020440e2de8ca7858ee7b4efbd9a09375
shyoon <dbstnsgh2@naver.com>
2022-12-20 11:24:01 +0900

    add config SecurityFilterChain

---
+++ src/main/java/com/example/studydanvegasecurity/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.studydanvegasecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Controller;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Controller
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(withDefaults())
+                .build();
+    }
+}
