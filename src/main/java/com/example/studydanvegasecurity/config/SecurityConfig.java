c0a5239bd0bbbd686fda65ffe6b880521f16940e
shyoon <dbstnsgh2@naver.com>
2022-12-20 10:37:18 +0900

    add config - InMemoryUserDetailsManager

--- src/main/java/com/example/studydanvegasecurity/config/SecurityConfig.java
+++ src/main/java/com/example/studydanvegasecurity/config/SecurityConfig.java
@@ -6,11 +6,38 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        /*
+        PasswordEncoder 를 설정하지 않을 경우 에러 발생
+        아래와 같이 PasswordEncoder 를 설정하거나
+        password 에 {noop} 를 추가하여 NoOpPasswordEncoder 로 설정한다.
+
+        하지만 NoOpPasswordEncoder 의 경우 원문 그대로 저장하기 때문에
+        학습용이 아닌 경우에는 권장하지는 않는다.
+         */
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername("user")
+                .password(encoder.encode("password"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(encoder.encode("password"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
