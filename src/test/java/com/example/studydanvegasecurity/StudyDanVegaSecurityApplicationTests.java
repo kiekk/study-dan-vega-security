0c62e79acd90f3c8a818d45029f1fc741aeed2e6
kiekk <kiekk@i-commtech.com>
2022-12-20 09:04:47 +0900

    Initial Commit

---
+++ src/test/java/com/example/studydanvegasecurity/StudyDanVegaSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studydanvegasecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDanVegaSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
