e2a917b73244126403e81a85c616fcaf2d2d3dde
shyoon <dbstnsgh2@naver.com>
2022-12-20 15:48:51 +0900

    add CommandLineRunner

--- src/main/java/com/example/studydanvegasecurity/StudyDanVegaSecurityApplication.java
+++ src/main/java/com/example/studydanvegasecurity/StudyDanVegaSecurityApplication.java
@@ -1,7 +1,11 @@
 package com.example.studydanvegasecurity;
 
+import com.example.studydanvegasecurity.entity.Post;
+import com.example.studydanvegasecurity.repository.PostRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StudyDanVegaSecurityApplication {
@@ -10,4 +14,10 @@ public class StudyDanVegaSecurityApplication {
         SpringApplication.run(StudyDanVegaSecurityApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(PostRepository repository) {
+        return args -> {
+            repository.save(new Post("Hello World!", "hello-world", "Welcome to my blog!", "User"));
+        };
+    }
 }
