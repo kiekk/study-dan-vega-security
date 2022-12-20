0501cb73087e99a183478565ef3f3a731584107e
shyoon <dbstnsgh2@naver.com>
2022-12-20 10:22:53 +0900

    add controller HomeController

---
+++ src/main/java/com/example/studydanvegasecurity/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.studydanvegasecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping(value = {"", "/"})
+    public String home() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "Hello, User!";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello, Admin!";
+    }
+
+}
