c3518d8b3c6d006f6b2559fb61ec923ab67a1e39
shyoon <dbstnsgh2@naver.com>
2022-12-20 14:03:16 +0900

    add @PreAuthorize to admin()

--- src/main/java/com/example/studydanvegasecurity/controller/HomeController.java
+++ src/main/java/com/example/studydanvegasecurity/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.studydanvegasecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class HomeController {
         return "Hello, World!";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public String admin() {
         return "Hello, Admin!";
