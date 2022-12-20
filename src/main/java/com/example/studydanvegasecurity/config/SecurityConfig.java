9da54113dc2ad3af28e6244862c7d24655b83644
shyoon <dbstnsgh2@naver.com>
2022-12-20 11:41:40 +0900

    add config InMemoryUserDetailsManager

--- src/main/java/com/example/studydanvegasecurity/config/SecurityConfig.java
+++ src/main/java/com/example/studydanvegasecurity/config/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.example.studydanvegasecurity.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Controller;
 
@@ -14,6 +15,16 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Controller
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user")
+                        .password("{noop}password")
+                        .authorities("read")
+                        .build()
+        );
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
