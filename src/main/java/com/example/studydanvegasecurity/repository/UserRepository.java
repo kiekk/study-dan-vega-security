9d73672b31f5a06944a827826420b5ec261a4a23
shyoon <dbstnsgh2@naver.com>
2022-12-20 16:02:46 +0900

    add JpaUserDetailsService

--- src/main/java/com/example/studydanvegasecurity/repository/UserRepository.java
+++ src/main/java/com/example/studydanvegasecurity/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.studydanvegasecurity.repository;
 import com.example.studydanvegasecurity.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
